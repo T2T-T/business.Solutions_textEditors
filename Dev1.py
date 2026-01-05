1aa17d2beefac6e30a2f806a3b844792845e1e0b
T2T-T <alans95@nycstudents.net>
2026-01-05 10:58:24 -0500

    dsda

---
+++ Dev1.py
@@ -0,0 +1,26 @@
+`from tkinter import *
+#need to install on all machines
+from tkmacosx import Button
+def 
+# Create the main window
+root = Tk()
+root.title("Enter Title Here")
+
+#Set size of window
+root.geometry("300x150")
+
+# Create buttons
+red_button = Button(root, text="Save", background='red')
+T = Text(root, height = 5, width = 52)
+T.pack()
+#Add a label
+
+label = Label(root, text="Save here")
+
+# Place widgets in window (with pack function!)
+label.pack()
+red_button.pack()
+
+# Start the GUI event loop
+root.mainloop()
+
