6b20b28b9266a7ef459b159604ad5cfcf905073e
T2T-T <alans95@nycstudents.net>
2026-01-06 11:28:47 -0500

    sad

--- Dev1.py
+++ Dev1.py
@@ -1,7 +1,8 @@
 `from tkinter import *
 #need to install on all machines
 from tkmacosx import Button
-def 
+def changeTitle():
+	root.title(T.get("1.0",END))
 # Create the main window
 root = Tk()
 root.title("Enter Title Here")
@@ -10,7 +11,7 @@ root.title("Enter Title Here")
 root.geometry("300x150")
 
 # Create buttons
-red_button = Button(root, text="Save", background='red')
+red_button = Button(root, text="Change Title", command=changeTitle)
 T = Text(root, height = 5, width = 52)
 T.pack()
 #Add a label
