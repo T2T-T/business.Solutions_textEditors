2a5a4a125bd5901cd915ef2de503d6cf1209aa9b
T2T-T <alans95@nycstudents.net>
2026-01-16 13:35:47 -0500

    updated for computer transfer

--- Reading_and_writing.py
+++ Reading_and_writing.py
@@ -1,14 +1,27 @@
 from tkinter import *
 #need to install on all machines
 from tkmacosx import Button
-def change_text():
+
+def write_file():
+	text_content = textbox.get("1.0",tk.END)
+
 	with open("file.txt","w") as f:
-		f.write("T")
+		f.write(text_content)
+
+		print("Content written to file.")
+
+def read_file():
+	with open("file.txt", "r") as f: 
+
+		content =f.read()
+		print(content)
+		textbox.delete("1.0",tk.END)
+		textbox.insert("1.0,content")
+
+
 # Create the main window
 root = Tk()
-root.title("Enter Title Here")
-
-#Set size of window
+root.title("Reading and wrtiting")
 
 # Create buttons
 red_button = Button(root, text="Red", background='red', command=change_text)
@@ -18,9 +31,6 @@ T = Text(root)
 # Place widgets in window (with pack function!)
 T.pack()
 red_button.pack()
-def read_file():
-with open("file.txt", "r") as f: 
-	content = f.read()
-	print(content)
+
 # Start the GUI event loop
 root.mainloop()
